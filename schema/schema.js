6c00c93408c8a4162daaee51161b3a471edbf3a2
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2018-10-17 13:08:27 -0400

    Update schema.js

--- schema/schema.js
+++ schema/schema.js
@@ -23,7 +23,7 @@ const ImageType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
-        images: {
+        image: {
             type: ImageType,
             args: { case_id: { type: GraphQLString } },
             resolve(parent, args){
